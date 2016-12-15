19084e10286bd741a8b6e1d2a691e3ec60106044
Eric DellAringa <edella2@gmail.com>
2016-12-14 19:48:08 -0600

    add album route, commas are important in models

--- app/models/album.js
+++ app/models/album.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   updated_at: DS.attr('date'),
-  total_sold: DS.attr('number')
+  total_sold: DS.attr('number'),
 
   comment_ids: DS.hasMany('comment'),
   artist_id: DS.belongsTo('artist')
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('artist', {path: '/artists/:artist_id'});
+  this.route('album', {path: '/ablums/:album_id'});
 });
 
 export default Router;
