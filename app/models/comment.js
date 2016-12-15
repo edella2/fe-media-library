7764dbdd658eb0561a621b6197c765783f6ea26d
Eric DellAringa <edella2@gmail.com>
2016-12-14 20:00:19 -0600

    create album show page and display comments, fix transform error in comment model

--- app/models/comment.js
+++ app/models/comment.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr('string'),
-  message: DS.attr('message'),
+  message: DS.attr('string'),
   updated_at: DS.attr('date'),
 
   album_id: DS.belongsTo('comment')
