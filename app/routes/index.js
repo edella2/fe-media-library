b09acab95004d4906fff377f21a4052185854804
Eric DellAringa <edella2@gmail.com>
2016-12-15 17:56:30 -0600

    Add unique year list and frequency hash of years

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,14 +9,49 @@ export default Ember.Route.extend({
       sort_direction: 'desc',
       reload: true
     }),
-    // using descending sort
+
     top_five_artists: this.store.findAll('artist', { reload: true }).
       then(artist => artist.sortBy('total_albums_sold').reverse().slice(0,5)),
 
-    albums: this.store.query('album', { reload: true}),
-    album_years: Ember.computed.mapBy('albums.@each.year', 'year')
-    });
+    years: this.get('store').findAll('album')
+      .then((albums) => {
+        let years = [];
+        let unique_years = [];
+        let years_objects = [];
 
+        albums.forEach((album) => {
+          years.push(album.get('year'));
+        });
+        years = years.sort((year1,year2) => {
+          return year1 - year2;
+        });
+
+        unique_years = years.filter((year, i, year_array) => {
+          return year_array.indexOf(year) === i;
+        });
+
+        years.forEach((year) => {
+          if(unique_years.indexOf(year) === -1) {
+            unique_years.push(year);
+          }
+        });
+
+        unique_years.forEach((unique_year) => {
+          let count = 0;
+          years.forEach((year) => {
+            if(year === unique_year) {
+              count += 1;
+            }
+          });
+          let year_object = {
+            year: unique_year,
+            count: count
+          };
+          years_objects.push(year_object);
+        });
+        return years_objects;
+      }),
+    });
   },
 
   queryParams: {
